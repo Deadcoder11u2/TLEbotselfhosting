8ca8b2fb7981d628a05622a58befb2ca83a23884
Srikanth Iyengar <ksrikanth3012@gmail.com>
2022-01-13 08:40:19 +0530

    change the run.sh file

--- tle/run.sh
+++ tle/run.sh
@@ -6,13 +6,11 @@ cd "$(dirname "$0")"
 [ -e environment ] && . ./environment
 
 while true; do
-    git pull
-    poetry install
     FONTCONFIG_FILE=$PWD/extra/fonts.conf poetry run python -m tle
-
     (( $? != 42 )) && break
 
     echo '==================================================================='
     echo '=                       Restarting                                ='
     echo '==================================================================='
+    
 done
